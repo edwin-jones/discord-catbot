c9fc779c6959d8e17acc96c83e4bb168af4f8788
Edwin Jones <edwinj85@gmail.com>
2018-05-07 01:44:08 +0100

    first pass at mongo stats

--- catbot.js
+++ catbot.js
@@ -5,8 +5,10 @@
 const discord = require('discord.io');
 const log = require('debug')('catbot')
 const request = require('request');
+const mongoClient = require('mongodb').MongoClient
+const StringBuilder = require('string-builder')
 
-const apiToken = process.env.DISCORD_API_TOKEN || "NOT_SET";
+const auth = require('./auth.json'); //you need to make this file yourself!
 
 function onError(bot, channelID) {
     bot.sendMessage(
@@ -14,6 +16,34 @@ function onError(bot, channelID) {
             to: channelID,
             message: "Sorry, I'm catnapping now. Please ask me later."
         });
+}
+
+//Use this function to incremement a stat
+function incrementStat(name)
+{
+    mongoClient.connect(auth.mongourl, function(err, database) {
+    
+        if (err)
+        {
+            log("failed to connect to mongodb with error: " + err);
+            return;
+        }
+
+        log("connected to mongodb");
+        
+        let connection = database.db('catbot');
+        let catstats = connection.collection('catstats');
+
+        catstats.update(
+            { name: name },
+            { $inc: { count: 1 } }
+        )
+    
+        database.close();
+
+        log(`incremented stat ${name} successfully`);
+
+  });  
 }
 
 //Use this function to post a cat fact into the relevant discord channel via the bot object.
@@ -30,6 +60,7 @@ function getCatFact(bot, channelID) {
                 message: body.fact
             });
 
+        incrementStat("catfacts");
         log("catfact command completed");
     });
 }
@@ -54,15 +85,68 @@ function getCatPic(bot, channelID, userID) {
                 }
         });
 
+        incrementStat("catpics");
         log("catpic command completed");
     });
 
 }
 
+//use this function to stroke catbot!
+function stroke(bot, channelID, userID) {
+    
+    bot.sendMessage(
+        {
+            to: channelID,
+            message: "**puuurrrrrrrrrr!** Thank you <@" + userID + "> **:3**"
+        });
+    
+    incrementStat("catstrokes");
+}
+
+
+//use this function to get stats
+function getCatStats(bot, channelID) {
+
+    mongoClient.connect(auth.mongourl, function(err, database) {
+    
+        if (err)
+        {
+            log("failed to connect to mongodb with error: " + err);
+            return;
+        }
+
+        log("connected to mongodb");
+        
+        let connection = database.db('catbot');
+        let catstats = connection.collection('catstats');
+
+        catstats.find({}).toArray(function(err, result) {
+            if (err) throw err;
+
+            var sb = new StringBuilder();
+            sb.appendLine("So far I have:")
+
+            for(var i in result)
+            {
+                sb.appendLine(`\t${result[i].prefix} **${result[i].count}** ${result[i].suffix}`);  
+            }
+
+            bot.sendMessage(
+                {
+                    to: channelID,
+                    message: sb.toString()
+                });
+            
+          });
+
+        database.close();
+    });  
+}
+
 // Initialize Discord Bot
 var bot = new discord.Client(
     {
-        token: apiToken,
+        token: auth.token,
         autorun: true
     });
 
@@ -96,7 +180,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage(
                     {
                         to: channelID,
-                        message: "you can ask me for a random cat fact with **!catfact**, picture with **!catpic** or you can stroke me with **!stroke** - I do love to be stroked **:3**"
+                        message: "You can ask me for a random cat fact with **!catfact**, picture with **!catpic** or you can stroke me with **!stroke** - I do love to be stroked **:3**\n" +
+                                 "I can also provide interesting cat fact stats with the **!catstats** command."
                     });
 
                 log("help command executed");
@@ -113,13 +198,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 break;
 
             case 'stroke':
-                bot.sendMessage(
-                    {
-                        to: channelID,
-                        message: "**puuurrrrrrrrrr!** Thank you <@" + userID + "> **:3**"
-                    });
-
+                stroke(bot, channelID, userID);
                 log("stroke command executed");
+                break;
+
+            case 'catstats':
+                getCatStats(bot, channelID);
+                log("catstats command executed");
                 break;
         }
     }
